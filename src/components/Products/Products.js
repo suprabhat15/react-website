3127c0af8c582a73964604cb99e08d1b9cc9d1fa
suprabhat25 <suprabhat2018@gmail.com>
2020-12-17 12:22:14 +0530

    init

---
+++ src/components/Products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from '../HeroSection'
+import { homeObjFour} from './Data'
+function Products() {
+    return (
+        <div>
+            <HeroSection {...homeObjFour} />
+        </div>
+    )
+}
+
+export default Products;
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { homeObjThree } from './Data'
+import HeroSection from '../HeroSection'
+import Pricing from '../Pricing'
+function SignUp() {
+    return (
+        <div>
+            <Pricing/>
+            <HeroSection {...homeObjThree} />
+        </div>
+    )
+}
+
+export default SignUp
